2214dfceacc8b883c197c6937176733ce8b7c37a
Will <williamjmason06@gmail.com>
2026-07-02 17:15:17 +0100

    changing graphing

--- cluster.py
+++ cluster.py
@@ -8,15 +8,17 @@ from sklearn.cluster import SpectralClustering
 from visualise import plot_embedding
 ids = np.load("data/movie_ids.npy")
 embeddings = np.load("data/movie_embeddings.npy")
-embeddings = embeddings[:100]
+#embeddings = embeddings[:100]
+
+
+chosen_ids = np.random.choice(ids[:100], size=10, replace=False)
 
 df_tmdb_movies = pd.read_csv("data/tmdb_movie_data.csv")
 df_letterboxd_ratings = pd.read_csv('data/letterboxd/ratings_withid.csv')
 
 
-
-clustering = SpectralClustering(n_clusters=2,
+clustering = SpectralClustering(n_clusters=5,
         assign_labels='discretize',
         random_state=0).fit(embeddings)
 
-plot_embedding('umap', clustering.labels_)
+plot_embedding('umap', clustering.labels_, chosen_ids)
--- visualise.py
+++ visualise.py
@@ -1,13 +1,20 @@
 import numpy as np
+import pandas as pd
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
 import umap
 import matplotlib.pyplot as plt
 
 embeddings = np.load("data/movie_embeddings.npy")
-embeddings = embeddings[:100]
+#embeddings = embeddings[:100]
 
-def plot_embedding(METHOD, labels):
+movie_ids = np.load("data/movie_ids.npy")
+
+df = pd.read_csv("data/tmdb_movie_data.csv")
+id_to_title = dict(zip(df["id"], df["title"]))
+
+
+def plot_embedding(METHOD, labels=None, highlight_id=None):
     match METHOD:
         case 'pca':
             pca = PCA(n_components=2)
@@ -19,9 +26,24 @@ def plot_embedding(METHOD, labels):
 
         case 'umap':
             reduced_embeddings = umap.UMAP().fit_transform(embeddings)
-
     
-
-    fig, ax = plt.subplots()
+    fig, ax = plt.subplots(figsize=(12,12))
     ax.scatter(reduced_embeddings[:,0], reduced_embeddings[:,1], marker='x', c=labels)
+    
+    if highlight_id is not None:
+
+        for id in highlight_id:
+
+            matches = np.where(movie_ids == id)[0]
+            idx = matches[0]
+
+            x = reduced_embeddings[idx, 0]
+            y = reduced_embeddings[idx, 1]
+
+            title = id_to_title.get(id, "Unknown")
+            ax.scatter(x,y, label=title, s=100)
+            
+
+
+    plt.legend()
     plt.show()
